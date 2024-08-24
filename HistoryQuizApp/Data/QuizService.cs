00242ccc816f9d2b0d1c92d26b240cf6e2c89210
Amit Kumar Upadhyay <aryan21211986@gmail.com>
2024-08-24 13:21:29 +0530

    Add a quiz page to the project. basic project set up updated

---
+++ HistoryQuizApp/Data/QuizService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HistoryQuizApp.Data
+{
+    public class QuizService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuizService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Quiz> GetQuizByIdAsync(int quizId)
+        {
+            return await _context.Quizzes
+                .Include(q => q.Questions)
+                .FirstOrDefaultAsync(q => q.Id == quizId);
+        }
+
+        public async Task SubmitAnswerAsync(int userId, int quizId, int questionId, string answer)
+        {
+            var userProgress = await _context.UserProgresses
+                .FirstOrDefaultAsync(up => up.UserId == userId && up.QuizId == quizId);
+
+            if (userProgress == null)
+            {
+                userProgress = new UserProgress
+                {
+                    UserId = userId,
+                    QuizId = quizId,
+                    Score = 0
+                };
+                _context.UserProgresses.Add(userProgress);
+            }
+
+            var question = await _context.Questions.FindAsync(questionId);
+            if (question.Answer == answer)
+            {
+                userProgress.Score += 1; // or any scoring logic
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> GetUserScoreAsync(int userId, int quizId)
+        {
+            var userProgress = await _context.UserProgresses
+                .FirstOrDefaultAsync(up => up.UserId == userId && up.QuizId == quizId);
+
+            return userProgress?.Score ?? 0;
+        }
+    }
+
+}
--- HistoryQuizApp/Program.cs
+++ HistoryQuizApp/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+builder.Services.AddScoped<QuizService>();
 
 var app = builder.Build();
 
